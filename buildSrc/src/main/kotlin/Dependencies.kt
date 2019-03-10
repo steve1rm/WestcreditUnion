f4ca0a1945f3db99894d9139cac51bfb1ec5d4f6
steve mason <stevekitkat@gmail.com>
2019-03-10 19:27:11 +0700

    working on kotlin-dsl

---
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -0,0 +1,8 @@
+object Plugins {
+    const val kotlinGradlePlugin = "org.jetbrains.kotlin:kotlin-gradle-plugin:${Versions.kotlin}"
+    const val androidGralePlugin = "com.android.tools.build:gradle:${Versions.gradle}"
+}
+
+object Libraries {
+    const val kotlinStdlib = "org.jetbrains.kotlin:kotlin-stdlib:${Versions.kotlin}"
+}
---
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -0,0 +1,19 @@
+
+object Versions {
+    /** Android */
+    const val buildToolsVersion = "28.0.3"
+    const val minSdkVersion = 15
+    const val targetSdkVersion = 27
+    const val compileSdkVersion = 28
+    const val versionCode = 1
+    const val versionName = "1.0"
+    const val kotlinVersion = "1.2.51"
+    const val gradleVersion = "3.3.2"
+
+    /** Testing */
+    const val jUnitVersion = "4.12"
+    const val assertJVersion = "3.11.1"
+    const val mockitoKotlinVersion = "1.6.0"
+    const val robolectricVersion = "3.4.2"
+    const val mockitoVersion = "1.9.5"
+}
